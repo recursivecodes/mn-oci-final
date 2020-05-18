c483d3901b1d474b6618bac3a013ca6ba1953699
recursivecodes <toddraymondsharp@gmail.com>
2020-05-18 10:58:01 -0400

    Revert "update to use distributed password"
    This reverts commit 1a03a45565aff81ea20eb16a717b2976d98faacc.

--- src/main/java/codes/recursive/Application.java
+++ src/main/java/codes/recursive/Application.java
@@ -5,7 +5,6 @@ import io.micronaut.runtime.Micronaut;
 public class Application {
 
     public static void main(String[] args) {
-        System.setProperty("oracle.jdbc.fanEnabled", "false");
         Micronaut.run(Application.class);
     }
 }
